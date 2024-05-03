e62d09eb7762a88b29f37e09a225cc49191ad320
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-05-04 00:17:20 +0500

    Add files via upload

---
+++ Quize_50/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// Question 50: 
+// Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. Include at least three different activities.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Using template literals to define a multiline string
+let myIdealDay = `My ideal day would involve:
+1. Waking up early.
+2. Spending a few hours coding typescript practice.
+3. Spending a time with family & friends.`;
+// Logging the multiline string to the console
+console.log(myIdealDay);
