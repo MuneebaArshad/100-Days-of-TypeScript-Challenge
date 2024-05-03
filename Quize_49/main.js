aa21b6ce769059f0132fa31eb9057a44c396af5d
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-05-04 00:17:00 +0500

    Add files via upload

---
+++ Quize_49/main.js
@@ -0,0 +1,14 @@
+"use strict";
+// Question 49: 
+// Function with Rest Parameters: Write a function that takes a rest parameter representing multiple hobbies. It should log each hobby with a statement saying you enjoy that hobby.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Defines a function that accepts multiple hobbies as arguments
+function logHobbies(...hobbies) {
+    // Loops through each hobby in the array
+    hobbies.forEach(hobby => {
+        // Logs a statement for each hobby
+        console.log(`I enjoy ${hobby}.`);
+    });
+}
+// Calls the function with three hobbies
+logHobbies("reading");
