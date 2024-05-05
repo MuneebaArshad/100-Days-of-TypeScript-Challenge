f5525d8aa941b5519cd6e296150c2a19b3419646
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-05-06 00:54:07 +0500

    Add files via upload

---
+++ Quize_62/main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Question 62:*
+//  Making a Student Template: Create a blueprint for student information, including their name, age, and the classes they're taking, and then fill in this blueprint with an example student.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Filling in the blueprint with an example student
+let student = {
+    name: "Ayan Arshad",
+    age: 22,
+    courses: ["python", "java", "c++"]
+};
+// Showing the student's information
+console.log(student);
