d8b1898094078570f07e54881bf50526a308e186
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-05-06 00:53:31 +0500

    Add files via upload

---
+++ Quize_61/main.js
@@ -0,0 +1,14 @@
+"use strict";
+// *Question 61:*
+//  Making Enums for Vehicles: Let's create categories for vehicles like cars, trucks, and motorcycles using enums, and show one example.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Making a list (enum) for different types of vehicles
+var VehicleType;
+(function (VehicleType) {
+    VehicleType[VehicleType["Car"] = 0] = "Car";
+    VehicleType[VehicleType["Truck"] = 1] = "Truck";
+    VehicleType[VehicleType["Motorcycle"] = 2] = "Motorcycle";
+})(VehicleType || (VehicleType = {}));
+// Showing one type of vehicle from the list
+console.log(VehicleType.Car); // It shows 0 because enums start counting from 0
+console.log(VehicleType); // it show all enums and counting
